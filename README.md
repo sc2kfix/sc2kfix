d7a3068414b1366975fdfc684a89fe99e2fa130f
araxestroy <troy@araxes.net>
2025-01-17 11:07:15 -0800

    Release 3
    - Add version detection
    - Add support for 1995 CD Collection
    - Add version resources for DLL

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # sc2kfix
-A bugfix DLL for SimCity 2000 Special Edition (1996)
+A bugfix DLL for SimCity 2000 Special Edition
 
 ## What does this do?
-This is a DLL that patches the 1996 Special Edition release of SimCity 2000 for Windows 95 to work properly on modern Windows systems. While the game itself is capable of running at high resolutions, oversights in the game's programming and techniques designed for use with 256-colour SVGA cards common in the mid 1990s cause problems with animations on truecolour displays. It also resolves the crashes that can occasionally happen with the load/save dialog boxes using a similar technique to Aleksander Krimsky's SC2000X patcher, but sc2kfix does so on in memory instead of creating a patched copy of your original EXE files.
+This is a DLL that patches the Special Edition (and 1995 CD Collection) release of SimCity 2000 for Windows 95 to work properly on modern Windows systems. While the game itself is capable of running at high resolutions, oversights in the game's programming and techniques designed for use with 256-colour SVGA cards common in the mid 1990s cause problems with animations on truecolour displays. It also resolves the crashes that can occasionally happen with the load/save dialog boxes using a similar technique to Aleksander Krimsky's SC2000X patcher, but sc2kfix does so on in memory instead of creating a patched copy of your original EXE files.
 
 The following issues are patched by this DLL:
 * Load/save dialog crash bug
@@ -12,4 +12,5 @@ The following issues are patched by this DLL:
 Simply copy the winmm.dll from the latest release to the same folder as simcity.exe, then start the game. If the lights in the game's splash screen are animated, it's working!
 
 ## To-do/known issues
-* There seems to be an earlier release of the Special Edition CD-ROM with an August 1995 timestamp. If someone has a copy of it please let me know so I can add support for it.
+* 1995 BUG: Clicking the "Load Saved City" option and then canceling the dialog box crashes the game
+* TODO: Fix animations in SCURK as well
--- dllmain.cpp
+++ dllmain.cpp
@@ -4,15 +4,23 @@
 #define GETPROC(i, name) fpHookList[i] = GetProcAddress(hRealWinMM, #name);
 #define DEFPROC(i, name) extern "C" __declspec(naked) void __stdcall _##name() { __asm { jmp fpHookList[i*4] }};
 
+#define VERSION_UNKNOWN 0
+#define VERSION_1995    1
+#define VERSION_1996    2
+
 #undef UNICODE
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <psapi.h>
+#include <stdio.h>
 
 #include "winmm_exports.h"
 
 HMODULE hRealWinMM = NULL;
+HMODULE hSC2KAppModule = NULL;
 FARPROC fpHookList[180] = { NULL };
+DWORD dwDetectedVersion = VERSION_UNKNOWN;
+DWORD dwSC2KAppTimestamp = 0;
 
 // This code replaces the original stack cleanup and return after the engine
 // cycles the animation palette.
@@ -35,6 +43,13 @@ BYTE bAnimationPatch1996[30] = {
     0x00, 0x5D, 0x5F, 0x5E, 0x5B, 0xC3
 };
 
+// Same as above, but with the offsets adjusted for the 1995 EXE
+BYTE bAnimationPatch1995[30] = {
+    0x68, 0x81, 0x00, 0x00, 0x00, 0x6A, 0x00, 0x6A, 0x00, 0x8B, 0x0D, 0x2C,
+    0x60, 0x4C, 0x00, 0x8B, 0x51, 0x1C, 0x52, 0xFF, 0x15, 0xE8, 0xEC, 0x4E,
+    0x00, 0x5D, 0x5F, 0x5E, 0x5B, 0xC3
+};
+
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved) {
     switch (reason) {
     case DLL_PROCESS_ATTACH:
@@ -53,7 +68,7 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved) {
         ALLEXPORTS(GETPROC);
 
 #ifdef DEBUG
-        MessageBox(GetActiveWindow(), "Hooked winmm.dll!", "sc2kfix", MB_OK | MB_ICONINFORMATION);
+        MessageBox(GetActiveWindow(), "DEBUG: Hooked winmm.dll!", "sc2kfix", MB_OK | MB_ICONINFORMATION);
 #endif
 
         // Make sure this isn't SCURK
@@ -62,27 +77,95 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved) {
         if (!_stricmp(szModuleBaseName, "winscurk.exe"))
             return true;
 
+        // Determine what version of SC2K this is
+        // HACK: there's probably a better way to do this
+        if (!(hSC2KAppModule = GetModuleHandle(NULL))) {
+            MessageBox(GetActiveWindow(), "Could not GetModuleHandle(NULL) (???)", "sc2kfix error", MB_OK | MB_ICONERROR);
+            return false;
+        }
+        dwSC2KAppTimestamp = ((PIMAGE_NT_HEADERS)(((PIMAGE_DOS_HEADER)hSC2KAppModule)->e_lfanew + (UINT_PTR)hSC2KAppModule))->FileHeader.TimeDateStamp;
+        switch (dwSC2KAppTimestamp) {
+        case 0x302FEA8A:
+            dwDetectedVersion = VERSION_1995;
+            break;
+
+        case 0x313E706E:
+            dwDetectedVersion = VERSION_1996;
+            break;
+
+        default:
+            dwDetectedVersion = VERSION_UNKNOWN;
+            char msg[300];
+            sprintf_s(msg, 300, "Could not detect SC2K version (got timestamp %08Xd). Your game will probably crash.\r\n\r\n"
+                "Please let us know in a GitHub issue what version of the game you're running so we can look into this.", dwSC2KAppTimestamp);
+            MessageBox(GetActiveWindow(), msg, "sc2kfix warning", MB_OK | MB_ICONWARNING);
+        }
+
+
         // Palette animation fix
+        LPVOID lpAnimationFix;
+        PBYTE lpAnimationFixSrc;
+        UINT uAnimationFixLength;
+        switch (dwDetectedVersion) {
+        case VERSION_1995:
+            lpAnimationFix = (LPVOID)0x00456B23;
+            lpAnimationFixSrc = bAnimationPatch1995;
+            uAnimationFixLength = 30;
+            break;
+
+        case VERSION_1996:
+        default:
+            lpAnimationFix = (LPVOID)0x004571D3;
+            lpAnimationFixSrc = bAnimationPatch1996;
+            uAnimationFixLength = 30;
+        }
         DWORD dummy;
-        VirtualProtect((LPVOID)0x004571D3, 30, PAGE_EXECUTE_READWRITE, &dummy);
-        memcpy((LPVOID)0x004571D3, bAnimationPatch1996, 30);
+        VirtualProtect(lpAnimationFix, uAnimationFixLength, PAGE_EXECUTE_READWRITE, &dummy);
+        memcpy(lpAnimationFix, lpAnimationFixSrc, uAnimationFixLength);
 
         // Dialog crash fix - hat tip to Aleksander Krimsky (@alekasm on GitHub)
-        VirtualProtect((LPVOID)0x004A04FA, 1, PAGE_EXECUTE_READWRITE, &dummy);
-        *(LPBYTE)0x004A04FA = 0x20;
-        VirtualProtect((LPVOID)0x004A0559, 2, PAGE_EXECUTE_READWRITE, &dummy);
-        *(LPBYTE)0x004A0559 = 0xEB;
-        *(LPBYTE)0x004A055A = 0xEB;
+        LPVOID lpDialogFix1;
+        LPVOID lpDialogFix2;
+        switch (dwDetectedVersion) {
+        case VERSION_1995:
+            lpDialogFix1 = (LPVOID)0x0049EE93;
+            lpDialogFix2 = (LPVOID)0x0049EEF2;
+            break;
+
+        case VERSION_1996:
+        default:
+            lpDialogFix1 = (LPVOID)0x004A04FA;
+            lpDialogFix2 = (LPVOID)0x004A0559;
+        }
+
+        VirtualProtect(lpDialogFix1, 1, PAGE_EXECUTE_READWRITE, &dummy);
+        *(LPBYTE)lpDialogFix1 = 0x20;
+        VirtualProtect(lpDialogFix2, 2, PAGE_EXECUTE_READWRITE, &dummy);
+        *(LPBYTE)lpDialogFix2 = 0xEB;
+        *(LPBYTE)((UINT_PTR)lpDialogFix2 + 1) = 0xEB;
 
         // Remove palette warnings
-        VirtualProtect((LPVOID)0x00408A79, 2, PAGE_EXECUTE_READWRITE, &dummy);
-        VirtualProtect((LPVOID)0x00408ABE, 18, PAGE_EXECUTE_READWRITE, &dummy);
-        *(LPBYTE)0x00408A79 = 0x90;
-        *(LPBYTE)0x00408A7A = 0x90;
-        memset((LPVOID)0x00408ABE, 0x90, 18);   // nop nop nop nop nop
+        LPVOID lpWarningFix1;
+        LPVOID lpWarningFix2;
+        switch (dwDetectedVersion) {
+        case VERSION_1995:
+            lpWarningFix1 = (LPVOID)0x00408749;
+            lpWarningFix2 = (LPVOID)0x0040878E;
+            break;
+
+        case VERSION_1996:
+        default:
+            lpWarningFix1 = (LPVOID)0x00408A79;
+            lpWarningFix2 = (LPVOID)0x00408ABE;
+        }
+        VirtualProtect(lpWarningFix1, 2, PAGE_EXECUTE_READWRITE, &dummy);
+        VirtualProtect(lpWarningFix2, 18, PAGE_EXECUTE_READWRITE, &dummy);
+        *(LPBYTE)lpWarningFix1 = 0x90;
+        *(LPBYTE)((UINT_PTR)lpWarningFix1 + 1) = 0x90;
+        memset((LPVOID)lpWarningFix2, 0x90, 18);   // nop nop nop nop nop
 
 #ifdef DEBUG
-        MessageBox(GetActiveWindow(), "Patched SC2K!", "sc2kfix", MB_OK | MB_ICONINFORMATION);
+        MessageBox(GetActiveWindow(), "DEBUG: Patched SC2K!", "sc2kfix", MB_OK | MB_ICONINFORMATION);
 #endif
 
     case DLL_THREAD_ATTACH:
