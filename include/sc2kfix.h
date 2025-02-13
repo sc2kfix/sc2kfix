f5c6a0da423e97b7c7ca02eba709d4adf5d9fe99
araxestroy <troy@araxes.net>
2025-02-13 11:34:03 -0800

    Bump version to Release 5

--- include/sc2kfix.h
+++ include/sc2kfix.h
@@ -16,7 +16,7 @@
 #define SC2KVERSION_1995    1
 #define SC2KVERSION_1996    2
 
-#define SC2KFIX_VERSION	"0.5-dev"
+#define SC2KFIX_VERSION	"0.5"
 
 #define RELATIVE_OFFSET(from, to) *(DWORD*)((DWORD)(from)) = (DWORD)(to) - (DWORD)(from) - 4;
 #define NEWCALL(from, to) *(BYTE*)(from) = 0xE8; RELATIVE_OFFSET((DWORD)(from)+1, to)
