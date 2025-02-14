339877aad8b14ee36d94ef80c41ea0a6a4b2e57b
araxestroy <troy@araxes.net>
2025-02-14 03:40:37 -0800

    Rewrite some dialog strings to be more explanatory

--- hooks/hook_miscellaneous.cpp
+++ hooks/hook_miscellaneous.cpp
@@ -32,9 +32,90 @@ static DWORD dwDummy;
 
 UINT iMilitaryBaseTries = 0;
 
+// Override some strings that have egregiously bad grammar/capitalization.
+// Maxis fail English? That's unpossible!
 extern "C" int __stdcall Hook_LoadStringA(HINSTANCE hInstance, UINT uID, LPSTR lpBuffer, int cchBufferMax) {
-	// This is mostly just a proof of concept showing we can arbitrarily intercept Win32 API calls.
-	// We'll use this for something useful later, I bet.
+	if (hInstance == hSC2KAppModule) {
+		switch (uID) {
+		case 108:
+			if (!strcpy_s(lpBuffer, cchBufferMax,
+				"Hydroelectric dams can only be placed on waterfall tiles."))
+				return strlen(lpBuffer);
+			break;
+		case 111:
+			if (!strcpy_s(lpBuffer, cchBufferMax,
+				"Tunnel cannot be built as it would intersect an existing tunnel."))
+				return strlen(lpBuffer);
+			break;
+		case 112:
+			if (!strcpy_s(lpBuffer, cchBufferMax,
+				"Tunnel cannot be built as it would leave the city limits."))
+				return strlen(lpBuffer);
+			break;
+		case 113:
+			if (!strcpy_s(lpBuffer, cchBufferMax,
+				"Tunnel cannot be built as it would be too deep in the terrain."))
+				return strlen(lpBuffer);
+			break;
+		case 114:
+			if (!strcpy_s(lpBuffer, cchBufferMax,
+				"Tunnel cannot be built as the exit terrain is unstable."))
+				return strlen(lpBuffer);
+			break;
+		case 115:
+			if (!strcpy_s(lpBuffer, cchBufferMax,
+				"An existing subway or sewer line is blocking construction."))
+				return strlen(lpBuffer);
+			break;
+		case 116:
+			if (!strcpy_s(lpBuffer, cchBufferMax,
+				"Tunnel entrances must be placed on a hillside."))
+				return strlen(lpBuffer);
+			break;
+		case 240:
+			if (!strcpy_s(lpBuffer, cchBufferMax,
+				"Your nation's military is interested in building a base on your city's soil."
+				"This could mean extra revenue. It could also raise new problems."
+				"Do you wish to grant land to the military?"))
+				return strlen(lpBuffer);
+			break;
+		case 289:
+			if (!strcpy_s(lpBuffer, cchBufferMax,
+				"Current rates are %d%%.\r\n"
+				"Do you wish to issue the bond?"))
+				return strlen(lpBuffer);
+			break;
+		case 290:
+			if (!strcpy_s(lpBuffer, cchBufferMax,
+				"You need $10,000 in cash to repay an outstanding bond."))
+				return strlen(lpBuffer);
+			break;
+		case 291:
+			if (!strcpy_s(lpBuffer, cchBufferMax,
+				"The oldest outstanding bond rate is %d%%.\r\n"
+				"Do you wish to repay this bond?"))
+				return strlen(lpBuffer);
+			break;
+		case 346:
+			if (!strcpy_s(lpBuffer, cchBufferMax,
+				"Engineers report that tunnel construction costs will be %s.\r\n"
+				"Do you wish to construct the tunnel?"))
+				return strlen(lpBuffer);
+			break;
+		case 745:
+			if (!strcpy_s(lpBuffer, cchBufferMax,
+				"Launch Arcology"))
+				return strlen(lpBuffer);
+			break;
+		case 32921:
+			if (!strcpy_s(lpBuffer, cchBufferMax,
+				"Saves city every 5 years"))
+				return strlen(lpBuffer);
+			break;
+		default:
+			return LoadStringA(hInstance, uID, lpBuffer, cchBufferMax);
+		}
+	}
 	return LoadStringA(hInstance, uID, lpBuffer, cchBufferMax);
 }
 
