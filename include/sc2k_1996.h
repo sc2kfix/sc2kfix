6693892ad289859573acdc3f838da2eb56863e3f
araxestroy <troy@araxes.net>
2025-02-14 02:31:56 -0800

    Add more game engine enums and structs

--- include/sc2k_1996.h
+++ include/sc2k_1996.h
@@ -25,6 +25,9 @@
 
 #define GAMEOFF_PTR GAMEOFF_ARR
 
+
+// Enums
+
 // Disaster IDs
 enum {
 	DISASTER_NONE = 0,
@@ -48,9 +51,81 @@ enum {
 	DISASTER_PLANECRASH
 };
 
+// Zone types
+enum {
+	ZONE_NONE = 0,
+	ZONE_LIGHT_RESIDENTIAL,
+	ZONE_DENSE_RESIDENTIAL,
+	ZONE_LIGHT_COMMERCIAL,
+	ZONE_DENSE_COMMERCIAL,
+	ZONE_LIGHT_INDUSTRIAL,
+	ZONE_DENSE_INDUSTRIAL,
+	ZONE_MILITARY,
+	ZONE_AIRPORT,
+	ZONE_SEAPORT
+};
+
+// Military base types
+enum {
+	MILITARY_BASE_NONE = 0,
+	MILITARY_BASE_DECLINED,
+	MILITARY_BASE_ARMY,
+	MILITARY_BASE_AIR_FORCE,
+	MILITARY_BASE_NAVY,
+	MILITARY_BASE_MISSILE_SILOS
+};
+
+
+// Structs
+
 typedef struct {
 	BYTE bDunno[32];
 } neighbor_city_t;
+
+typedef struct {
+	struct {
+		WORD iDunno : 6;
+		WORD iWaterLevel : 5;
+		WORD iLandAltitude : 5;
+	} w[128];
+} map_ALTM_t;
+
+typedef struct {
+	struct {
+		BYTE iZoneType : 4;
+		BYTE iCorners : 4;
+	} b[128];
+} map_XZON_t;
+
+typedef struct {
+	BYTE iBuildingID[128];
+} map_XBLD_t;
+
+typedef struct {
+	BYTE bDunno[128];
+} map_XTER_t;
+
+typedef struct {
+	BYTE iTileID[128];
+} map_XUND_t;
+
+typedef struct {
+	BYTE bTextOverlay[128];
+} map_XTXT_t;
+
+typedef struct {
+	struct {
+		BYTE iPowerable : 1;
+		BYTE iPowered : 1;
+		BYTE iPiped : 1;
+		BYTE iWatered : 1;
+		BYTE iXVALMask : 1;
+		BYTE iWater : 1;
+		BYTE iRotated : 1;
+		BYTE iSaltWater : 1;
+	} b[128];
+} map_XBIT_t;
+
 
 // Pointers
 
@@ -61,7 +136,7 @@ GAMEOFF(DWORD,	dwCityFunds,				0x4CA444)
 GAMEOFF(WORD,	wCityStartYear,				0x4CA5F4)
 GAMEOFF(DWORD,	dwCityPopulation,			0x4CAA74)
 GAMEOFF(DWORD,	dwInScenario,				0x4CAD44)
-GAMEOFF_ARR(neighbor_city_t, stNeighborCities, 0x4CAD58)
+GAMEOFF_ARR(neighbor_city_t, stNeighborCities, 0x4CAD58)	// stNeighborCities[4]
 GAMEOFF(DWORD,	dwCityDays,					0x4CAE04)
 GAMEOFF(WORD,	wCityProgression,			0x4CB010)
 GAMEOFF(WORD,	wCityCurrentMonth,			0x4CB01C)
@@ -82,3 +157,13 @@ GAMEOFF_ARR(DWORD, dwZoneNameStringIDs,		0x4E7140)
 GAMEOFF_ARR(DWORD, dwCityNoticeStringIDs,	0x4E98B8)
 GAMEOFF(DWORD,	dwCityRewardsUnlocked,		0x4E9A24)
 
+
+// Pointers to map arrays
+
+GAMEOFF_ARR(map_XTER_t,	dwMapXTER,	0x4C9F58)
+GAMEOFF_ARR(map_XZON_t,	dwMapXZON,	0x4CA1F0)
+GAMEOFF_ARR(map_XTXT_t,	dwMapXTXT,	0x4CA600)
+GAMEOFF_ARR(map_XBIT_t,	dwMapXBIT,	0x4CAB10)
+GAMEOFF_ARR(map_ALTM_t,	dwMapALTM,	0x4CAE10)
+GAMEOFF_ARR(map_XUND_t,	dwMapXUND,	0x4CB1D0)
+GAMEOFF_ARR(map_XBLD_t,	dwMapXBLD,	0x4CC4F0)
