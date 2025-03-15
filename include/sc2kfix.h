e3d41f4d8b702b6f19f9107d91c830c9c30e98b5
araxestroy <troy@araxes.net>
2025-03-14 17:37:14 -0700

    Release 9a

--- include/sc2kfix.h
+++ include/sc2kfix.h
@@ -23,8 +23,8 @@
 #define SC2KVERSION_1995    1
 #define SC2KVERSION_1996    2
 
-#define SC2KFIX_VERSION		"0.10-dev"
-#define SC2KFIX_RELEASE_TAG	"r9"
+#define SC2KFIX_VERSION		"0.9a"
+#define SC2KFIX_RELEASE_TAG	"r9a"
 
 #define RELATIVE_OFFSET(from, to) *(DWORD*)((DWORD)(from)) = (DWORD)(to) - (DWORD)(from) - 4;
 #define NEWCALL(from, to) *(BYTE*)(from) = 0xE8; RELATIVE_OFFSET((DWORD)(from)+1, to)
