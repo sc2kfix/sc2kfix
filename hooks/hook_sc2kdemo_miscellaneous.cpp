05012c4a13436278c57f0c9222b898efedd39d65
tdm <af-az@users.noreply.github.com>
2025-07-06 00:05:01 +0100

    Applied the 'Arial' font and sign fix to the Interactive Demo.

--- hooks/hook_sc2kdemo_miscellaneous.cpp
+++ hooks/hook_sc2kdemo_miscellaneous.cpp
@@ -18,6 +18,17 @@
 
 #pragma intrinsic(_ReturnAddress)
 
+static DWORD dwDummy;
+
 void InstallMiscHooks_SC2KDemo(void) {
 	InstallRegistryPathingHooks_SC2KDemo();
+
+	// Fix the 'Arial" font
+	VirtualProtect((LPVOID)0x4CF130, 6, PAGE_EXECUTE_READWRITE, &dwDummy);
+	memset((LPVOID)0x4CF130, 0, 6);
+	memcpy_s((LPVOID)0x4CF130, 6, "Arial", 6);
+	VirtualProtect((LPVOID)0x4403A3, 1, PAGE_EXECUTE_READWRITE, &dwDummy);
+	*(BYTE*)0x4403A3 = 5;
+	VirtualProtect((LPVOID)0x4403AE, 1, PAGE_EXECUTE_READWRITE, &dwDummy);
+	*(BYTE*)0x4403AE = 10;
 }
