dbfb2c680189eb6d89782b7f631dca3352968c23
tdm <af-az@users.noreply.github.com>
2025-11-21 03:39:54 +0000

    Added the 'Open Main Dialog' menu item to the initial File menu to ensure you can continue to use the program even if the dialog fails to appear the first time round. Fixed the filename criteria for the load city/tileset and save dialogues. Accounted for a program crash that would occur if the Load City dialogue were cancelled prior to a game being started. (1995 CD Collection)

--- include/sc2k_1995.h
+++ include/sc2k_1995.h
@@ -46,6 +46,7 @@
 CC1995_GAMECALL(0x40103C, void, __thiscall, MainFrame_ToggleToolBars, CMainFrame *, BOOL)
 CC1995_GAMECALL(0x4014F1, int, __thiscall, SimcityApp_ExitRequester, CSimcityAppPrimary *, int)
 CC1995_GAMECALL(0x401587, void, __thiscall, SimcityApp_SaveCity, CSimcityAppPrimary *)
+CC1995_GAMECALL(0x4022C0, int, __stdcall, UpdateSectionsAndResetWindowMenu)
 CC1995_GAMECALL(0x4026D0, CSimcityView *, __thiscall, SimcityApp_PointerToCSimcityViewClass, CSimcityAppPrimary *)
 
 // Main
@@ -53,6 +54,13 @@ CC1995_GAMECALL_MAIN(0x456A60, void, __cdecl, ToggleColorCycling, CMFC3XPalette 
 CC1995_GAMECALL_MAIN(0x4A9467, void, __thiscall, WinApp_OnAppExit, CMFC3XWinApp *)
 
 // MFC and WinAPI
+CC1995_GAMECALL_MAIN(0x4A1700, BOOL, __thiscall, CmdTarget_OnCmdMsg, CMFC3XCmdTarget *, UINT nID, int nCode, void *pExtra, void *pHandlerInfo)
+CC1995_GAMECALL_MAIN(0x4A2AD3, CMFC3XWnd *, __stdcall, Wnd_FromHandle, HWND hWnd)
+CC1995_GAMECALL_MAIN(0x4A2AF1, CMFC3XWnd *, __stdcall, Wnd_FromHandlePermanent, HWND)
+CC1995_GAMECALL_MAIN(0x4A4209, CMFC3XTestCmdUI *, __thiscall, TestCmdUI_Construct, CMFC3XTestCmdUI *)
+CC1995_GAMECALL_MAIN(0x4A4F85, BOOL, __thiscall, Wnd_SendChildNotifyLastMsg, CMFC3XWnd *, LRESULT *)
+CC1995_GAMECALL_MAIN(0x4B8B8E, BOOL, __thiscall, FrameWnd_OnCmdMsg, CMFC3XFrameWnd *, UINT nID, int nCode, void *pExtra, void *pHandlerInfo)
+CC1995_GAMECALL_MAIN(0x4BF624, MFC3X_AFX_THREAD_STATE *, __stdcall, AfxGetThreadState, void)
 
 // Vars
 
