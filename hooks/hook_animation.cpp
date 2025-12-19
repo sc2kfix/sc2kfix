39c4639c3be874c87d4ca0329b430fd53e15ec19
tdm <af-az@users.noreply.github.com>
2025-12-19 07:38:08 +0000

    Reimplemented the colour-cycling hook in full. (SC2KDemo). Also if the game speed is paused and the programstep is in-game.. don't instigate a colour-cycle redraw.

--- hooks/hook_animation.cpp
+++ hooks/hook_animation.cpp
@@ -52,6 +52,11 @@ extern "C" void __cdecl Hook_ToggleColorCycling_SC2K1996(CMFC3XPalette *pPalette
 					GameMain_DC_SelectPalette(pDC, pSelPal, FALSE);
 					ReleaseDC(pMainFrm->m_hWnd, pDC->m_hDC);
 
+					// Let's not.
+					if (pSCApp->wSCAGameSpeedLOW == GAME_SPEED_PAUSED &&
+						pSCApp->iSCAProgramStep == ONIDLE_STATE_INGAME)
+						bRedraw = FALSE;
+
 					if (bRedraw) {
 						pMapToolBar = &pMainFrm->dwMFMapToolBar;
 						pCityToolBar = &pMainFrm->dwMFCityToolBar;
@@ -146,54 +151,73 @@ extern "C" void __cdecl Hook_ToggleColorCycling_SC2K1995(CMFC3XPalette *pPalette
 }
 
 extern "C" void __cdecl Hook_ToggleColorCycling_SC2KDemo(CMFC3XPalette *pPalette, BOOL bToggle) {
-	CSimcityAppDemo *pApp;
+	CSimcityAppDemo *pSCApp;
 	CMainFrame *pMainFrm;
+	HDC hDC;
+	CMFC3XDC *pDC;
+	CMFC3XPalette *pSelPal;
 	CSimcityView *pSCView;
 	CMapToolBar *pMapToolBar;
 	CCityToolBar *pCityToolBar;
-	WORD wSimSpeed;
-	DWORD dwTitleScreenAnimation;
-	int iProgramStep;
-	BOOL bCityViewAnim;
+	BOOL bRedraw, bCityViewAnim;
 
-	GameMain_ToggleColorCycling_Demo(pPalette, bToggle);
-
-	pApp = &pCSimcityAppThis_Demo;
-	if (pApp) {
-		if (!bLoColor_Demo) {
-			pMainFrm = (CMainFrame *)pApp->m_pMainWnd; // m_pMainWnd
-			wSimSpeed = pApp->wSCAGameSpeedLOW;
-			dwTitleScreenAnimation = pApp->dwSCAToggleTitleScreenAnimation;
-			iProgramStep = pApp->iSCAProgramStep;
-			if (wSimSpeed != GAME_SPEED_PAUSED || dwTitleScreenAnimation || iProgramStep != ONIDLE_STATE_INGAME) {
+	pSCApp = &pCSimcityAppThis_Demo;
+	if (pSCApp) {
+		if (pSCApp->wSCAGameSpeedLOW != GAME_SPEED_PAUSED || pSCApp->dwSCAToggleTitleScreenAnimation || pSCApp->iSCAProgramStep != ONIDLE_STATE_INGAME) {
+			if (!bLoColor_Demo) {
+				pMainFrm = (CMainFrame *)pSCApp->m_pMainWnd;
 				if (pMainFrm) {
-					pMapToolBar = &pMainFrm->dwMFMapToolBar;
-					pCityToolBar = &pMainFrm->dwMFCityToolBar;
+					GetPaletteEntries((HPALETTE)pPalette->m_hObject, 0, 0x100, pPalAnimMain_Demo);
+					hDC = GetDC(pMainFrm->m_hWnd);
+					pDC = GameMain_DC_FromHandle_Demo(hDC);
+					pSelPal = GameMain_DC_SelectPalette_Demo(pDC, pPalette, FALSE);
+					if (bToggle) {
+						Game_SwapCycle_Demo(0);
+						AnimatePalette((HPALETTE)pPalette->m_hObject, 224, 16, pPalOffCycle_Demo);
+						bRedraw = TRUE;
+					}
+					else {
+						Game_SwapCycle_Demo(1);
+						AnimatePalette((HPALETTE)pPalette->m_hObject, 171, 49, pPalOnCycle_Demo);
+						bRedraw = TRUE;
+					}
+					GameMain_DC_SelectPalette_Demo(pDC, pSelPal, FALSE);
+					ReleaseDC(pMainFrm->m_hWnd, pDC->m_hDC);
 
-					// With this check, the redraw calls won't be made if either toolbar is being dragged.
-					// This avoids any bleeding that may occur as a result of the blitted border that will
-					// appear during this time.
-					//
-					// NOTE: The only side-effect is that during toolbar dragging no palette animation will
-					//       occur; this won't have any effect on the simulation itself since that is temporarily
-					//       suspended during city toolbar dragging (and it doesn't matter during the map toolbar
-					//       dragging case).
-					bCityViewAnim = TRUE;
+					// Let's not.
+					if (pSCApp->wSCAGameSpeedLOW == GAME_SPEED_PAUSED &&
+						pSCApp->iSCAProgramStep == DEMO_ONIDLE_STATE_INGAME)
+						bRedraw = FALSE;
 
-					if (pCityToolBar && pCityToolBar->dwCTBToolBarTitleDrag)
-						bCityViewAnim = FALSE;
+					if (bRedraw) {
+						pMapToolBar = &pMainFrm->dwMFMapToolBar;
+						pCityToolBar = &pMainFrm->dwMFCityToolBar;
 
-					if (pMapToolBar && pMapToolBar->dwMTBToolBarTitleDrag)
-						bCityViewAnim = FALSE;
+						// With this check, the redraw calls won't be made if either toolbar is being dragged.
+						// This avoids any bleeding that may occur as a result of the blitted border that will
+						// appear during this time.
+						//
+						// NOTE: The only side-effect is that during toolbar dragging no palette animation will
+						//       occur; this won't have any effect on the simulation itself since that is temporarily
+						//       suspended during city toolbar dragging (and it doesn't matter during the map toolbar
+						//       dragging case).
+						bCityViewAnim = TRUE;
 
-					// CMainFrame m_hWnd - only call this specific redraw function before CSimcityView has been created.
-					// (ie, before any game has been started - palette animation on the image is disabled once the
-					// game window has been created)
-					pSCView = Game_SimcityApp_PointerToCSimcityViewClass_Demo(pApp);
-					if (!pSCView)
-						RedrawWindow(pMainFrm->m_hWnd, NULL, NULL, RDW_INVALIDATE);
-					else if (pSCView && bCityViewAnim)
-						RedrawWindow(pSCView->m_hWnd, NULL, NULL, RDW_INVALIDATE);
+						if (pCityToolBar && pCityToolBar->dwCTBToolBarTitleDrag)
+							bCityViewAnim = FALSE;
+
+						if (pMapToolBar && pMapToolBar->dwMTBToolBarTitleDrag)
+							bCityViewAnim = FALSE;
+
+						// CMainFrame m_hWnd - only call this specific redraw function before CSimcityView has been created.
+						// (ie, before any game has been started - palette animation on the image is disabled once the
+						// game window has been created)
+						pSCView = Game_SimcityApp_PointerToCSimcityViewClass_Demo(pSCApp);
+						if (!pSCView)
+							RedrawWindow(pMainFrm->m_hWnd, NULL, NULL, RDW_INVALIDATE);
+						else if (pSCView && bCityViewAnim)
+							RedrawWindow(pSCView->m_hWnd, NULL, NULL, RDW_INVALIDATE);
+					}
 				}
 			}
 		}
--- include/sc2k_demo.h
+++ include/sc2k_demo.h
@@ -43,9 +43,10 @@
 
 // Thunk
 DEMO_GAMECALL(0x402725, CSimcityView *, __thiscall, SimcityApp_PointerToCSimcityViewClass, CSimcityAppDemo *)
+DEMO_GAMECALL(0x402A09, void, __cdecl, SwapCycle, int)
 
 // Main
-DEMO_GAMECALL_MAIN(0x44890F, void, __cdecl, ToggleColorCycling, CMFC3XPalette *, BOOL)
+
 
 // MFC and WinAPI
 DEMO_GAMECALL_MAIN(0x48BF14, CMFC3XWnd *, __stdcall, Wnd_FromHandle, HWND hWnd)
@@ -53,14 +54,18 @@ DEMO_GAMECALL_MAIN(0x48BF32, CMFC3XWnd *, __stdcall, Wnd_FromHandlePermanent, HW
 DEMO_GAMECALL_MAIN(0x48D64A, CMFC3XTestCmdUI *, __thiscall, TestCmdUI_Construct, CMFC3XTestCmdUI *)
 DEMO_GAMECALL_MAIN(0x48E3C6, BOOL, __thiscall, Wnd_SendChildNotifyLastMsg, CMFC3XWnd *, LRESULT *)
 DEMO_GAMECALL_MAIN(0x48F069, BOOL, __thiscall, CmdTarget_OnCmdMsg, CMFC3XCmdTarget *, UINT nID, int nCode, void *pExtra, void *pHandlerInfo)
+DEMO_GAMECALL_MAIN(0x494036, CMFC3XDC *, __stdcall, DC_FromHandle, HDC)
+DEMO_GAMECALL_MAIN(0x4944E8, CMFC3XPalette *, __thiscall, DC_SelectPalette, CMFC3XDC *, CMFC3XPalette *, BOOL)
 DEMO_GAMECALL_MAIN(0x4A2207, BOOL, __thiscall, FrameWnd_OnCmdMsg, CMFC3XFrameWnd *, UINT nID, int nCode, void *pExtra, void *pHandlerInfo)
 DEMO_GAMECALL_MAIN(0x4A9858, void, __thiscall, WinApp_EnableShellOpen, CMFC3XWinApp *)
 DEMO_GAMECALL_MAIN(0x4AA2B1, MFC3X_AFX_THREAD_STATE *, __stdcall, AfxGetThreadState, void)
 
 // Vars
-
-DEMO_GAMEOFF(CSimcityAppDemo,	pCSimcityAppThis,			0x4B6A70)
-DEMO_GAMEOFF(BOOL,	bCSimcityDocSC2InUse,		0x4D15D4)
-DEMO_GAMEOFF(BOOL,	bCSimcityDocSCNInUse,		0x4D15D8)
-DEMO_GAMEOFF(DWORD,	dwUnknownInitVarOne,		0x4D15DC)
-DEMO_GAMEOFF(BOOL,	bLoColor,			0x4D1EDC)
+DEMO_GAMEOFF_ARR(PALETTEENTRY,	pPalAnimMain,			0x4B13D8)
+DEMO_GAMEOFF_ARR(PALETTEENTRY,	pPalOnCycle,			0x4B17D8)
+DEMO_GAMEOFF_ARR(PALETTEENTRY,	pPalOffCycle,			0x4B1CA8)
+DEMO_GAMEOFF(CSimcityAppDemo,	pCSimcityAppThis,		0x4B6A70)
+DEMO_GAMEOFF(BOOL,	bCSimcityDocSC2InUse,				0x4D15D4)
+DEMO_GAMEOFF(BOOL,	bCSimcityDocSCNInUse,				0x4D15D8)
+DEMO_GAMEOFF(DWORD,	dwUnknownInitVarOne,				0x4D15DC)
+DEMO_GAMEOFF(BOOL,	bLoColor,							0x4D1EDC)
