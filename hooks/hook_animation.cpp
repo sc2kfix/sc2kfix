670c98549274f4a1961f0847358f61c68e4245de
tdm <af-az@users.noreply.github.com>
2025-12-19 07:18:54 +0000

    Reimplemented the colour-cycling hook in full in order to attempt to reduce the number of unnecessary redraw cases. (SC2K1996SE)

--- hooks/hook_animation.cpp
+++ hooks/hook_animation.cpp
@@ -13,62 +13,77 @@
 static DWORD dwDummy;
 
 extern "C" void __cdecl Hook_ToggleColorCycling_SC2K1996(CMFC3XPalette *pPalette, int bToggle) {
-	CSimcityAppPrimary *pApp;
+	CSimcityAppPrimary *pSCApp;
 	CMainFrame *pMainFrm;
+	HDC hDC;
+	CMFC3XDC *pDC;
+	CMFC3XPalette *pSelPal;
 	CSimcityView *pSCView;
 	CMapToolBar *pMapToolBar;
 	CCityToolBar *pCityToolBar;
-	WORD wSimSpeed;
-	DWORD dwTitleScreenAnimation;
-	int iProgramStep;
-	BOOL bCityViewAnim;
+	BOOL bRedraw, bCityViewAnim;
 
 	// Only redraw the relevant windows during:
 	// 1) Titlescreen image animation.
 	// 2) While the CSimcityView window is active and the toolbars aren't being dragged.
 	// 3) None of the additional redraw calls in LoColor mode.
 
-	GameMain_ToggleColorCycling(pPalette, bToggle);
-
-	pApp = &pCSimcityAppThis;
-	if (pApp) {
-		if (!bLoColor) {
-			pMainFrm = (CMainFrame *)pApp->m_pMainWnd;
-			wSimSpeed = pApp->wSCAGameSpeedLOW;
-			dwTitleScreenAnimation = pApp->dwSCAToggleTitleScreenAnimation;
-			iProgramStep = pApp->iSCAProgramStep;
-			if (wSimSpeed != GAME_SPEED_PAUSED || dwTitleScreenAnimation || iProgramStep != ONIDLE_STATE_INGAME) {
+	bRedraw = FALSE;
+	pSCApp = &pCSimcityAppThis;
+	if (pSCApp) {
+		if (pSCApp->wSCAGameSpeedLOW != GAME_SPEED_PAUSED || pSCApp->dwSCAToggleTitleScreenAnimation || pSCApp->iSCAProgramStep != ONIDLE_STATE_INGAME) {
+			if (!bLoColor) {
+				pMainFrm = (CMainFrame *)pSCApp->m_pMainWnd;
 				if (pMainFrm) {
-					pMapToolBar = &pMainFrm->dwMFMapToolBar;
-					pCityToolBar = &pMainFrm->dwMFCityToolBar;
+					GetPaletteEntries((HPALETTE)pPalette->m_hObject, 0, 0x100, pPalAnimMain);
+					hDC = GetDC(pMainFrm->m_hWnd);
+					pDC = GameMain_DC_FromHandle(hDC);
+					pSelPal = GameMain_DC_SelectPalette(pDC, pPalette, FALSE);
+					if (bToggle) {
+						Game_SwapCycle(0);
+						AnimatePalette((HPALETTE)pPalette->m_hObject, 224, 16, pPalOffCycle);
+						bRedraw = TRUE;
+					}
+					else {
+						Game_SwapCycle(1);
+						AnimatePalette((HPALETTE)pPalette->m_hObject, 171, 49, pPalOnCycle);
+						bRedraw = TRUE;
+					}
+					GameMain_DC_SelectPalette(pDC, pSelPal, FALSE);
+					ReleaseDC(pMainFrm->m_hWnd, pDC->m_hDC);
 
-					// With this check, the redraw calls won't be made if either toolbar is being dragged.
-					// This avoids any bleeding that may occur as a result of the blitted border that will
-					// appear during this time.
-					//
-					// NOTE: The only side-effect is that during toolbar dragging no palette animation will
-					//       occur; this won't have any effect on the simulation itself since that is temporarily
-					//       suspended during city toolbar dragging (and it doesn't matter during the map toolbar
-					//       dragging case).
-					bCityViewAnim = TRUE;
+					if (bRedraw) {
+						pMapToolBar = &pMainFrm->dwMFMapToolBar;
+						pCityToolBar = &pMainFrm->dwMFCityToolBar;
 
-					if (pCityToolBar && pCityToolBar->dwCTBToolBarTitleDrag)
-						bCityViewAnim = FALSE;
-					
-					if (pMapToolBar && pMapToolBar->dwMTBToolBarTitleDrag)
-						bCityViewAnim = FALSE;
+						// With this check, the redraw calls won't be made if either toolbar is being dragged.
+						// This avoids any bleeding that may occur as a result of the blitted border that will
+						// appear during this time.
+						//
+						// NOTE: The only side-effect is that during toolbar dragging no palette animation will
+						//       occur; this won't have any effect on the simulation itself since that is temporarily
+						//       suspended during city toolbar dragging (and it doesn't matter during the map toolbar
+						//       dragging case).
+						bCityViewAnim = TRUE;
 
-					if (CanUseFloatingStatusDialog() && bStatusDialogMoving)
-						bCityViewAnim = FALSE;
+						if (pCityToolBar && pCityToolBar->dwCTBToolBarTitleDrag)
+							bCityViewAnim = FALSE;
 
-					// CMainFrame m_hWnd - only call this specific redraw function before CSimcityView has been created.
-					// (ie, before any game has been started - palette animation on the image is disabled once the
-					// game window has been created)
-					pSCView = Game_SimcityApp_PointerToCSimcityViewClass(pApp);
-					if (!pSCView)
-						RedrawWindow(pMainFrm->m_hWnd, NULL, NULL, RDW_INVALIDATE);
-					else if (pSCView && bCityViewAnim)
-						RedrawWindow(pSCView->m_hWnd, NULL, NULL, RDW_INVALIDATE);
+						if (pMapToolBar && pMapToolBar->dwMTBToolBarTitleDrag)
+							bCityViewAnim = FALSE;
+
+						if (CanUseFloatingStatusDialog() && bStatusDialogMoving)
+							bCityViewAnim = FALSE;
+
+						// CMainFrame m_hWnd - only call this specific redraw function before CSimcityView has been created.
+						// (ie, before any game has been started - palette animation on the image is disabled once the
+						// game window has been created)
+						pSCView = Game_SimcityApp_PointerToCSimcityViewClass(pSCApp);
+						if (!pSCView)
+							RedrawWindow(pMainFrm->m_hWnd, NULL, NULL, RDW_INVALIDATE);
+						else if (pSCView && bCityViewAnim)
+							RedrawWindow(pSCView->m_hWnd, NULL, NULL, RDW_INVALIDATE);
+					}
 				}
 			}
 		}
