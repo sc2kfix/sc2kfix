47c8cb19afc404bbe9ddc57ba61c3a71c347db9b
tdm <af-az@users.noreply.github.com>
2026-05-25 02:31:26 +0100

    Of course.

--- hooks/hook_spritesandtilesets.cpp
+++ hooks/hook_spritesandtilesets.cpp
@@ -904,7 +904,7 @@ extern "C" void __stdcall Hook_SimcityApp_LoadTileset1996() {
 
 	Game_SimcityApp_GetValueStringA(pThis, &strFilePath, aPaths, aTilesets);
 
-	strcpy(fileDialog.m_szFileName, strFilePath.m_pchData);
+	old_strcpy(fileDialog.m_szFileName, strFilePath.m_pchData);
 	pOfn = &fileDialog.m_ofn;
 	pOfn->lpstrInitialDir = fileDialog.m_szFileName;
 	pOfn->lpstrTitle = strCaption.m_pchData;
