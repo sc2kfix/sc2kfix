9227fc4a80bc9505d613f224174d900bfd432246
tdm <af-az@users.noreply.github.com>
2026-06-03 23:31:34 +0100

    Add the initial scenario hook installation call.

---
+++ hooks/hook_scenario.cpp
@@ -0,0 +1,20 @@
+// sc2kfix hook/hook_scenario.cpp: hooks for Scenario functionality
+// (c) 2026 sc2kfix project (https://sc2kfix.net) - released under the MIT license
+
+#undef UNICODE
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <intrin.h>
+#include <list>
+#include <map>
+#include <string>
+
+#include <sc2kfix.h>
+#include "../resource.h"
+
+#pragma intrinsic(_ReturnAddress)
+
+void InstallScenarioHooks_SC2K1996(void) {
+
+}
