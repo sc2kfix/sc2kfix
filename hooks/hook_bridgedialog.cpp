dc16b6a644f158d61270705298b72874277b983e
tdm <af-az@users.noreply.github.com>
2026-06-24 07:48:28 +0100

    Add in the initial bridge dialog hook source.

---
+++ hooks/hook_bridgedialog.cpp
@@ -0,0 +1,18 @@
+// sc2kfix hooks/hook_bridgedialog.cpp: hooks for the bridge dialog
+// (c) 2026 sc2kfix project (https://sc2kfix.net) - released under the MIT license
+
+#undef UNICODE
+#include <windows.h>
+#include <windowsx.h>
+#include <psapi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <intrin.h>
+#include <string>
+
+#include <sc2kfix.h>
+#include "../resource.h"
+
+void InstallBridgeDialogHooks_SC2K1996(void) {
+
+}
